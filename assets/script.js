6ba3974290b443f0e71951fa2092959c80d490eb
GoldPineapple <67350143+Underpi@users.noreply.github.com>
2023-07-24 17:55:19 -0700

    scroll functionality
    added scroll functionality to be configured later

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const links = document.querySelectorAll('a');
+// const observer = new IntersectionObserver(entries => {
+//   // Loop over the entries
+//   entries.forEach(entry => {
+//     // If the element is visible
+//     if (entry.isIntersecting) {
+//       // Add the animation class
+//       entry.target.classList.add('square-animation');
+//     }
+//   });
+// });
 
-  links.forEach(link => {
-    link.addEventListener('click', smoothScroll);
-  });
-
-  function smoothScroll(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute('href');
-    const targetPosition = document.querySelector(targetId).offsetTop;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition - startPosition;
-    const duration = 800;
-    let start = null;
-
-    function animation(currentTime) {
-      if (start === null) start = currentTime;
-      const progress = currentTime - start;
-      window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-      if (progress < duration) requestAnimationFrame(animation);
-    }
-
-    function easeInOutCubic(t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return c / 2 * t * t * t + b;
-      t -= 2;
-      return c / 2 * (t * t * t + 2) + b;
-    }
-
-    requestAnimationFrame(animation);
-  }
-});
+// observer.observe(document.querySelector('.square'));
